1729cc0c42bf5659e16ce380250564922d3dd764
Himal bhusal <mememimi413@gmail.com>
2025-03-01 19:15:56 +0545

    refactor fixing auth

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: `${import.meta.env.VITE_API_KEY}`,
-  authDomain: "https://code-editor-ebon-theta.vercel.app/",
+  authDomain: `${import.meta.env.VITE_AUTHDOMAIN}`,
   projectId: `${import.meta.env.VITE_PROJECTID}`,
   storageBucket: `${import.meta.env.VITE_STORAGEBUCKET}`,
   messagingSenderId: `${import.meta.env.VITE_MSGSENDERID}`,
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+// const db = getFirestore(app)
 const auth = getAuth(app)
 // const analytics = getAnalytics(app);
-export {app,db,auth}
+export {app,auth}
